4d8acd3952053f864f1b863014a49e4f8d9a222f
suheonjoo <suheonj95@naver.com>
2022-12-31 17:55:22 +0900

    update shell scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,22 +4,22 @@
 
 
 # 가동중인 awsstudy 도커 중단 및 삭제
-sudo docker ps -a -q --filter "name=jenkeinsfreestyle" | grep -q . && docker stop jenkeinsfreestyle && docker rm jenkeinsfreestyle | true
+docker ps -a -q --filter "name=jenkeinsfreestyle" | grep -q . && docker stop jenkeinsfreestyle && docker rm jenkeinsfreestyle | true
 
 # 기존 이미지 삭제
-sudo docker rmi bluelaw/jenkeinsfreestyle:1.0;
+docker rmi bluelaw/jenkeinsfreestyle:1.0;
 
-sudo docker rm jenkeinsfreestyle
+docker rm jenkeinsfreestyle
 
-sudo docker build -t bluelaw/jenkeinsfreestyle:1.0 .
+docker build -t bluelaw/jenkeinsfreestyle:1.0 .
 
 # 도커 run  -v /home/ec2-user:/config
-sudo docker run -d -p 8080:8080 --name jenkeinsfreestyle bluelaw/jenkeinsfreestyle:1.0
+docker run -d -p 8080:8080 --name jenkeinsfreestyle bluelaw/jenkeinsfreestyle:1.0
 
-sudo docker start jenkeinsfreestyle
+docker start jenkeinsfreestyle
 
 # 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제 안됨
-sudo docker rmi -f $(docker images -f "dangling=true" -q) || true
+docker rmi -f $(docker images -f "dangling=true" -q) || true
 
 
 echo "Done!!!!!!!!!  feel good"
