13767dcfe72dfb31b4ab9b44966794bb056e10ce
suheonjoo <suheonj95@naver.com>
2022-12-31 21:18:48 +0900

    update scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,36 +3,58 @@
 
 
 
-# 가동중인 awsstudy 도커 중단 및 삭제
-echo "11111111"
-#docker ps -a -q --filter "name=jenkeinsfreestyle" | grep -q . && docker stop jenkeinsfreestyle && docker rm jenkeinsfreestyle | true
-
-
 echo "2222"
 # 기존 이미지 삭제
-docker rmi bluelaw/jenkeinsfreestyle:1.0;
+sudo docker rmi bluelaw/jenkeinsfreestyle:1.0;
 
 echo "3333"
-docker stop jenkeinsfreestyle
+sudo docker stop jenkeinsfreestyle
 
 echo "4444"
-docker rm jenkeinsfreestyle
+sudo docker rm jenkeinsfreestyle
 
 echo "5555"
-docker build -t bluelaw/jenkeinsfreestyle:1.0 .
+sudo docker build -t bluelaw/jenkeinsfreestyle:1.0 .
 
 echo "6666"
 # 도커 run  -v /home/ec2-user:/config
-docker run -p 8080:8080 --name jenkeinsfreestyle bluelaw/jenkeinsfreestyle:1.0
-
+sudo docker run -p 8080:8080 --name jenkeinsfreestyle bluelaw/jenkeinsfreestyle:1.0
+# 여기서 -d 옵션을 주면 백그라운드에서 실행하는 거라서 spring 이 시작할때 나오는 로고.. 출력문들을 볼수 없다
 
 echo "7777"
-#docker start jenkeinsfreestyle
-#
-#
-#echo "8888"
-# 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제 안됨
 
 
 
 echo "Done!!!!!!!!!  feel good"
+
+
+#
+#
+#echo "2222"
+## 기존 이미지 삭제
+#docker rmi bluelaw/jenkeinsfreestyle:1.0;
+#
+#echo "3333"
+#docker stop jenkeinsfreestyle
+#
+#echo "4444"
+#docker rm jenkeinsfreestyle
+#
+#echo "5555"
+#docker build -t bluelaw/jenkeinsfreestyle:1.0 .
+#
+#echo "6666"
+## 도커 run  -v /home/ec2-user:/config
+#docker run -p 8080:8080 --name jenkeinsfreestyle bluelaw/jenkeinsfreestyle:1.0
+## 여기서 -d 옵션을 주면 백그라운드에서 실행하는 거라서 spring 이 시작할때 나오는 로고.. 출력문들을 볼수 없다
+#
+#echo "7777"
+##docker start jenkeinsfreestyle
+##
+##
+##echo "8888"
+## 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제 안됨
+#
+#
+#
+#echo "Done!!!!!!!!!  feel good"
