74803ec0f6b9894cf87b1cedff4511f18352e56f
suheonjoo <suheonj95@naver.com>
2022-12-31 17:34:57 +0900

    update shell scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,9 +10,10 @@ grep -q . && docker stop jenkeinsfreestyle && docker rm jenkeinsfreestyle | true
 # 기존 이미지 삭제
 sudo docker rmi bluelaw/jenkeinsfreestyle:1.0;
 
+docker build -t bluelaw/jenkeinsfreestyle:1.0 .
 
-# 도커 run
-docker run -d -p 8080:8080 -v /home/ec2-user:/config --name jenkeinsfreestyle bluelaw/jenkeinsfreestyle:1.0
+# 도커 run  -v /home/ec2-user:/config
+docker run -d -p 8080:8080 --name jenkeinsfreestyle bluelaw/jenkeinsfreestyle:1.0
 
 # 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제 안됨
 docker rmi -f $(docker images -f "dangling=true" -q) || true
