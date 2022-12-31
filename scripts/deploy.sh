23efa55c94cf127622ea1528f149c3e1f8ce0caa
suheonjoo <suheonj95@naver.com>
2022-12-31 17:16:24 +0900

    update shell scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
 
+
+
+# 가동중인 awsstudy 도커 중단 및 삭제
+sudo docker ps -a -q --filter "name=jenkeinsfreestyle" | \
+grep -q . && docker stop jenkeinsfreestyle && docker rm jenkeinsfreestyle | true
+
+# 기존 이미지 삭제
+sudo docker rmi bluelaw/jenkeinsfreestyle:1.0;
+
+
+# 도커 run
+docker run -d -p 8080:8080 -v /home/ec2-user:/config --name jenkeinsfreestyle bluelaw/jenkeinsfreestyle:1.0
+
+# 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제 안됨
+docker rmi -f $(docker images -f "dangling=true" -q) || true
+
+
 echo "Done!!!!!!!!!  feel good"
