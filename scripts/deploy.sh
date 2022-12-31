ebce176bc1ad8624adf2ca9e52e578991a0866da
suheonjoo <suheonj95@naver.com>
2022-12-31 18:00:55 +0900

    update shell scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -29,7 +29,7 @@ docker start jenkeinsfreestyle
 
 echo "8888"
 # 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제 안됨
-docker rmi -f $(docker images -f "dangling=true" -q) || true
+
 
 
 echo "Done!!!!!!!!!  feel good"
